8b60249a993d6e41aac0a709b7fcae862c4d95ba
Chin-Chang Yang <chin.chang.yang@gmail.com>
2013-01-03 16:06:06 +0800

    Redefine lumberjack version

--- src/nl/mightydev/version/LumberjackVersion.java
+++ src/nl/mightydev/version/LumberjackVersion.java
@@ -1,27 +1,30 @@
 package nl.mightydev.version;
 
-import nl.mightydev.lumberjack.util.Number;
-
 public class LumberjackVersion {
-	public final MinecraftVersion minecraft;
-	public final int revision;
+	public final String value;
 	
 	public LumberjackVersion(String version) {
-		String parts[] = version.split("v");
-		boolean valid = parts.length == 2;
-		minecraft = new MinecraftVersion(valid ? parts[0] : ""); 
-		revision = valid ? Number.parse(parts[1], -1) : -1;
-	}
-	
-	public boolean equals(Object other) {
-		return other instanceof LumberjackVersion && equals((LumberjackVersion)other);
-	}
-	
-	public boolean equals(LumberjackVersion other) {
-		return revision == other.revision;
+		value = version;
 	}
 
 	public String toString() {
-		return minecraft + "v" + (revision != -1 ? revision : "invalid Lumberjack version");
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LumberjackVersion other = (LumberjackVersion) obj;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
 	}
 }
