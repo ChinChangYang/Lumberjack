3c3046850d0e26e738bfe9b437ce760a9f86202e
Chin-Chang Yang <chin.chang.yang@gmail.com>
2013-01-10 10:30:36 +0800

    Add LumberjackPermissions.

---
+++ src/nl/mightydev/lumberjack/LumberjackPermissions.java
@@ -0,0 +1,10 @@
+package nl.mightydev.lumberjack;
+
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+
+
+public class LumberjackPermissions {
+
+	public static Permission ALL = new Permission("lumberjack.all", PermissionDefault.OP);
+}
