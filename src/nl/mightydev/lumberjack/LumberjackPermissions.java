2a4cb737d591e9b2b058d37d8296aceed7969cf2
Chin-Chang Yang <chin.chang.yang@gmail.com>
2013-01-16 16:48:06 +0800

    Support PermissionsBukkit.

--- src/nl/mightydev/lumberjack/LumberjackPermissions.java
+++ src/nl/mightydev/lumberjack/LumberjackPermissions.java
@@ -8,7 +8,13 @@ public class LumberjackPermissions {
 	private final static String ALL = "lumberjack.all";
 	
 	public static boolean check(Permissible p) {
-		if (!Plugin.manager.isPluginEnabled("PermissionsEx")) return true;
+		if (!isPermissionPluginEnabled()) return true;
 		else return p.hasPermission(ALL);
+	}
+	
+	private static boolean isPermissionPluginEnabled() {
+		if (Plugin.manager.isPluginEnabled("PermissionsEx")) return true;
+		if (Plugin.manager.isPluginEnabled("PermissionsBukkit")) return true;
+		return false;
 	}
 }
