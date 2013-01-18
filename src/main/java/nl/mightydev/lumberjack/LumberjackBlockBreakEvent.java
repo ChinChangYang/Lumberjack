4f73b0d11ed668b85ca1c1460c82af29784d66ae
Chin-Chang Yang <chin.chang.yang@gmail.com>
2013-01-18 18:28:37 +0800

    Make this a Maven project.

---
+++ src/main/java/nl/mightydev/lumberjack/LumberjackBlockBreakEvent.java
@@ -0,0 +1,13 @@
+package nl.mightydev.lumberjack;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class LumberjackBlockBreakEvent extends BlockBreakEvent {
+
+	public LumberjackBlockBreakEvent(Block theBlock, Player player) {
+		super(theBlock, player);
+	}
+
+}
